75e73012c71b5a770bf1fea5f29478a49a254fec
7777766666 <112616064+7777766666@users.noreply.github.com>
2022-12-11 00:06:50 +0300

    Add files via upload
    Делаем шапку

--- src/main/java/ru/kata/spring/boot_security/demo/Init.java
+++ src/main/java/ru/kata/spring/boot_security/demo/Init.java
@@ -46,7 +46,7 @@ public class Init {
             User adminUser1 = new User("adminUser", "MegaAdminAndUser", 111, "au",  //$2a$12$/V8QItpaDlzt62enZ6jy2ufUPmPfwcmxrBT2M0f903zBDKL3imX6i
                     "$2a$12$RTR7TtmVoD1ZYS/tttyWJ.ShHH6ivsn9OnInWQoZbCmds3eReD/2y", setRole3);
 
-            User user1 = new User("user", "MainUser", 55, "a",                   //"user@gmail.com",  $2a$12$QEP7TBCJvY2BIA1ez4uSE.TOfSayI40I8.fv/gRZHczVN2H/uqOMq
+            User user1 = new User("user", "MainUser", 55, "u",                   //"user@gmail.com",  $2a$12$QEP7TBCJvY2BIA1ez4uSE.TOfSayI40I8.fv/gRZHczVN2H/uqOMq
                     "$2a$12$qMD1dXg4/kXleOBqLfTzsOf8muowYIBAaUp8BnQZITaYzdwKfvRFi", setRole2);
 
             User admin1 = new User("admin", "MainAdmin", 99, "a",               //"admin@gmail.com",  "$2a$12$cMXTYl25dfJ616oWo5CjhOsZIXYdQSDyIorgNrYN4cCn4PLrVKwjC"
